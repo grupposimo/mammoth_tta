e42e611cc905709281ca328cf64bcbd09f0a5fa9
apanariello4 <nellopanariello96@gmail.com>
2024-02-13 13:12:13 +0100

    Add train_on_source_dataset function to utils/tta.py

--- utils/training.py
+++ utils/training.py
@@ -22,6 +22,7 @@ from utils import random_id
 from utils.checkpoints import mammoth_load_checkpoint
 from utils.loggers import *
 from utils.status import ProgressBar
+from utils.tta import train_on_source_dataset
 
 try:
     import wandb
@@ -80,7 +81,7 @@ def evaluate(model: ContinualModel, dataset: ContinualDataset, last=False) -> Tu
                 break
             inputs, labels = data
             inputs, labels = inputs.to(model.device), labels.to(model.device)
-            if 'class-il' not in model.COMPATIBILITY and 'general-continual' not in model.COMPATIBILITY and 'continual-tta' not in model.COMPATIBILITY:
+            if all(setting not in model.COMPATIBILITY for setting in ('class-il', 'general-continual', 'continual-tta')):
                 outputs = model(inputs, k)
             else:
                 outputs = model(inputs)
@@ -155,45 +156,7 @@ def train(model: ContinualModel, dataset: ContinualDataset,
         print('Checkpoint Loaded!')
 
     if args.train_source and not args.loadcheck and dataset.SETTING == 'continual-tta':
-        opt = optim.AdamW(model.parameters(), lr=args.lr)
-        sched = torch.optim.lr_scheduler.CosineAnnealingLR(opt, args.n_epochs)
-        criterion = dataset.get_source_loss()
-        model.train()
-        train_set, test_set = dataset.get_source_dataset()
-        train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=4)
-        test_loader = torch.utils.data.DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=4)
-        for e in range(model.args.n_epochs):
-            with tqdm(total=len(train_loader), desc=f"[EP{e}] Training on source dataset") as pbar:
-                for i, data in enumerate(train_loader):
-                    if args.debug_mode and i > model.get_debug_iters():
-                        break
-                    inputs, labels = data
-                    inputs, labels = inputs.to(model.device), labels.to(model.device, dtype=torch.long)
-                    outputs = model(inputs)
-                    loss = criterion(outputs, labels)
-                    opt.zero_grad()
-                    loss.backward()
-                    opt.step()
-                    pbar.update()
-                    pbar.set_postfix({'loss': loss.item(), 'lr': opt.param_groups[0]['lr']})
-            sched.step()
-            total = 0
-            correct = 0
-            model.eval()
-            with torch.no_grad():
-                for i, data in tqdm(enumerate(test_loader), desc=f"[EP{e}] Testing on source dataset", total=len(test_loader)):
-                    if args.debug_mode and i > model.get_debug_iters():
-                        break
-                    inputs, labels = data
-                    inputs, labels = inputs.to(model.device), labels.to(model.device, dtype=torch.long)
-                    outputs = model(inputs)
-                    _, predicted = torch.max(outputs, 1)
-                    total += labels.size(0)
-                    correct += (predicted == labels).sum().item()
-                print(f"EP{e} acc = {correct/total:.2%}")
-            Path(f'./data/checkpoints').mkdir(parents=True, exist_ok=True)
-            torch.save(model.state_dict(), f'./data/checkpoints/{args.dataset}_{e}_source.pt')
-            print(f"Source task accuracy: {correct/total:.2%}, saved to ./data/checkpoints/{args.dataset}_{e}_source.pt")
+        train_on_source_dataset(model, dataset, args)
 
     progress_bar = ProgressBar(joint=args.joint, verbose=not args.non_verbose)
 
--- utils/tta.py
+++ utils/tta.py
@@ -1,5 +1,8 @@
+from pathlib import Path
 import torch
 import torch.nn as nn
+from tqdm import tqdm
+
 
 class EntropyLoss(nn.Module):
     def __init__(self):
@@ -7,3 +10,45 @@ class EntropyLoss(nn.Module):
 
     def forward(self, x):
         return torch.mean(-(x.softmax(1) * x.log_softmax(1)).sum(1), 0)
+
+
+def train_on_source_dataset(model, dataset, args):
+    opt = torch.optim.AdamW(model.parameters(), lr=args.lr)
+    sched = torch.optim.lr_scheduler.CosineAnnealingLR(opt, args.n_epochs)
+    criterion = dataset.get_source_loss()
+    model.train()
+    train_set, test_set = dataset.get_source_dataset()
+    train_loader = torch.utils.data.DataLoader(train_set, batch_size=args.batch_size, shuffle=True, num_workers=4)
+    test_loader = torch.utils.data.DataLoader(test_set, batch_size=args.batch_size, shuffle=False, num_workers=4)
+    for e in range(model.args.n_epochs):
+        with tqdm(total=len(train_loader), desc=f"[EP{e}] Training on source dataset") as pbar:
+            for i, data in enumerate(train_loader):
+                if args.debug_mode and i > model.get_debug_iters():
+                    break
+                inputs, labels = data
+                inputs, labels = inputs.to(model.device), labels.to(model.device, dtype=torch.long)
+                outputs = model(inputs)
+                loss = criterion(outputs, labels)
+                opt.zero_grad()
+                loss.backward()
+                opt.step()
+                pbar.update()
+                pbar.set_postfix({'loss': loss.item(), 'lr': opt.param_groups[0]['lr']})
+        sched.step()
+        total = 0
+        correct = 0
+        model.eval()
+        with torch.no_grad():
+            for i, data in tqdm(enumerate(test_loader), desc=f"[EP{e}] Testing on source dataset", total=len(test_loader)):
+                if args.debug_mode and i > model.get_debug_iters():
+                    break
+                inputs, labels = data
+                inputs, labels = inputs.to(model.device), labels.to(model.device, dtype=torch.long)
+                outputs = model(inputs)
+                _, predicted = torch.max(outputs, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        print(f"EP{e} acc = {correct/total:.2%}")
+    Path(f'./data/checkpoints').mkdir(parents=True, exist_ok=True)
+    torch.save(model.state_dict(), f'./data/checkpoints/{args.dataset}_{e}_source.pt')
+    print(f"Source task accuracy: {correct/total:.2%}, saved to ./data/checkpoints/{args.dataset}_{e}_source.pt")
